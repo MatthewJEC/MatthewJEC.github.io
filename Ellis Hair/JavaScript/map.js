2ad6e52922fb7a32470022c4a1104cb617f162c8
[MatthewJEC] <[matthewjecampbell@gmail.com]>
2022-01-07 11:42:49 +0000

    updated directory tree

--- Ellis Hair/JavaScript/map.js
+++ Ellis Hair/JavaScript/map.js
@@ -4,7 +4,7 @@ function initMap() {
     const mayfield = { lat: 55.876155, lng: -3.039505 }
     map = new google.maps.Map(document.getElementById("map"), {
     center: mayfield,
-    zoom: 18,
+    zoom: 17,
   });
     const marker = new google.maps.Marker({
         position: mayfield,
