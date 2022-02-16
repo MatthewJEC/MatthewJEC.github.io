a46d6e44045caab0645b110231bf5be91b09f06b
[MatthewJEC] <[matthewjecampbell@gmail.com]>
2022-02-16 12:23:00 +0000

    adding technologies and minor tweaks

--- Ellis Hair/JS/map.js
+++ Ellis Hair/JS/map.js
@@ -4,11 +4,44 @@ function initMap() {
     const mayfield = { lat: 55.876155, lng: -3.039505 }
     map = new google.maps.Map(document.getElementById("map"), {
     center: mayfield,
-    zoom: 15.5,
+    zoom: 16,
+  });
+
+  const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Ellis Thom Hair Studio</h1>' +
+    '<div id="bodyContent">' +
+    "<h2>1 Ashgrove<br> " +
+    "Mayfield<br> " +
+    "Midlothian<br> " +
+    "EH22 5DA</h2>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
   });
     const marker = new google.maps.Marker({
         position: mayfield,
         map: map,
         title:"1 Ashgrove, Mayfield"
     });
+
+    // let openMarker = () => {
+    //   infowindow.open({
+    //     anchor: marker,
+    //     map,
+    //     shouldFocus: false,
+    //   });
+    // });
+    // }
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: false,
+      });
+    });
+    // openMarker;
 }
